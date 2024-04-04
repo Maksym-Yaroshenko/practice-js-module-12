bb0690c2075d46a99b76bd8a604cbd86eec9b3a5
Maksym-Yaroshenko <yiaroshenko.max@gmail.com>
2024-04-04 21:53:13 +0300

    update

--- js/main.js
+++ js/main.js
@@ -28,3 +28,31 @@ try {
 }
 
 console.log("After try...catch");
+
+a();
+
+function fact(n) {
+  if (n <= 1) return 1;
+
+  // console.log(n);
+  return n * fact(n - 1);
+}
+
+const data = fact(170);
+
+console.log(data);
+
+a();
+
+function fibba(n) {
+  if (n <= 1) return 1;
+
+  const data = fibba(n - 1) + fibba(n - 2);
+  // console.log(data);
+  return data;
+}
+
+// 1 1 2 3 5 8 13 21
+// 0 1 2 3 4 5 6  7
+
+console.log(fibba(50));
